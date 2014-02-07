dae118a5a9453fd710c68de469acc9a5949a8071
Wes Moser <Moser.Wesley@gmail.com>
2014-02-06 20:32:02 -0500

    Initial Commit

---
+++ Quiz4/Quiz4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz4AppDelegate.h
+//  Quiz4
+//
+//  Created by Moser, Wesley on 2/6/14.
+//  Copyright (c) 2014 Moser, Wesley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz4
+//
+//  Created by Moser, Wesley on 2/6/14.
+//  Copyright (c) 2014 Moser, Wesley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Quiz4AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Quiz4AppDelegate class]));
+    }
+}
